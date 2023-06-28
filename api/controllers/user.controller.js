99f17e1a4411937d21fb4ea038425b063abd61bf
[Yel Yan] <[kmin2957@gmail.com]>
2023-06-28 09:02:21 +0300

    upload api authentication

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+const deleteUser = (req, res) => {
+   return res.send("Pann Pan delete")
+}
+
+module.exports = deleteUser;
+
---
+++ api/utils/handleErrors.js
@@ -0,0 +1,36 @@
+function handleErrors (err) {
+    // console.log(err.message , err.code);
+    let errors = {
+        username : "",
+        email : "",
+        password : ""
+    };
+
+    // incorrect mail & password for login validation
+    if(err.message === "Incorrect email"){
+        errors.email = err.message;
+        return errors;
+    }
+    if(err.message === "Incorrect password"){
+        errors.password = err.message;
+        return errors;
+    }
+
+    // duplicate key error means same email register
+    if(err.code === 11000){
+        errors.username = "This username is already registered! Please Try another one";
+        errors.email = "This email is already registered";
+        return errors;
+    }
+
+    // email and password validation
+    if(err.message.includes("User validation failed")){
+        Object.values(err.errors).forEach(x => {
+            let {properties } = x;
+            errors[properties.path] = properties.message;
+        })
+    }
+    return errors;
+}
+
+module.exports = handleErrors;
